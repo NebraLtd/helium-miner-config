1b7ac56ffe13cda33f7567945fb248a6145ad364
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-15 17:21:27 +0000

    Conversion

--- wifiTests.py
+++ wifiTests.py
@@ -4,7 +4,7 @@ from pprint import pprint
 try:
     print(nmcli.connection())
     print(nmcli.device.show('wlan0')['GENERAL.STATE'].split(" ")[0])
-    print(nmcli.device.wifi() )
+    pprint(nmcli.device.wifi() )
 
     #nmcli.connection.delete('Doge')
 except Exception as e:
