b10ebc61853b9578f5c091d92bdfcd9e27f2e1a4
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-16 14:05:42 +0000

    Bump

--- dbusTests.py
+++ dbusTests.py
@@ -2,5 +2,5 @@ import dbus
 
 miner_bus = dbus.SystemBus()
 miner_object = miner_bus.get_object('com.helium.Miner', '/')
-miner_interface = dbus.Interface(p2pSatus, 'com.helium.Miner')
+miner_interface = dbus.Interface(miner_object, 'com.helium.Miner')
 miner_interface.P2PStatus()
