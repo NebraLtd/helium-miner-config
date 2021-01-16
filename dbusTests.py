97b045c977b9f6c433891e28f923b6ab25241153
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-16 19:05:36 +0000

    Add more debug

--- dbusTests.py
+++ dbusTests.py
@@ -1,6 +1,9 @@
 import dbus
+from time import sleep
 
 miner_bus = dbus.SystemBus()
 miner_object = miner_bus.get_object('com.helium.Miner', '/')
+sleep(0.1)
 miner_interface = dbus.Interface(miner_object, 'com.helium.Miner')
-miner_interface.P2PStatus()
+sleep(0.1)
+print(miner_interface.P2PStatus())
