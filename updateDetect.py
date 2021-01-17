cb716b0817aed0be9ba1fabc433070b4f522a3d4
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-17 13:47:46 +0000

    Test program

---
+++ updateDetect.py
@@ -0,0 +1,7 @@
+import urllib.request, json, os
+supervisorAddress = str(os.environ['BALENA_SUPERVISOR_ADDRESS'])
+supervisorKey = str(os.environ['BALENA_SUPERVISOR_API_KEY'])
+supervisorAddress = "%s/v2/applications/state?apikey=%s" % supervisorAddress, supervisorKey
+with urllib.request.urlopen(balenaState) as url:
+    data = json.loads(url.read().decode())
+    print(data)
