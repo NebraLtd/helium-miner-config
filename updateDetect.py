fbf9af0359a7577fba05008458970051c7403fe2
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-17 13:50:08 +0000

    Fix url

--- updateDetect.py
+++ updateDetect.py
@@ -2,6 +2,6 @@ import urllib.request, json, os
 supervisorAddress = str(os.environ['BALENA_SUPERVISOR_ADDRESS'])
 supervisorKey = str(os.environ['BALENA_SUPERVISOR_API_KEY'])
 supervisorAddress = "%s/v2/applications/state?apikey=%s" % (supervisorAddress, supervisorKey)
-with urllib.request.urlopen(balenaState) as url:
+with urllib.request.urlopen(supervisorAddress) as url:
     data = json.loads(url.read().decode())
     print(data)
