e824c42ab7d9d468fa8ef0518e05d2fd711f3a89
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-19 17:15:30 +0000

    Change advertisement ID

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class ConfigAdvertisement(Advertisement):
         Advertisement.__init__(self, index, "peripheral")
         macAddressTrimmed = ""
         macAddressTrimmed = open("/sys/class/net/eth0/address").readline().strip().replace(":","")[-4:].upper()
-        localName = "Helium Hotspot %s" % (macAddressTrimmed)
+        localName = "Nebra Hotspot %s" % (macAddressTrimmed)
         self.add_local_name(localName)
         self.include_tx_power = True
         self.service_uuids = ["0fda92b2-44a2-4af2-84f5-fa682baa2b8d"]
