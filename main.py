bda31b351c6e84518270435c07e21273bdba7837
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-29 19:58:46 +0000

    Re add mac

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ class ConfigAdvertisement(Advertisement):
     def __init__(self, index):
         Advertisement.__init__(self, index, "peripheral")
         variant = os.getenv('VARIANT')
-        localName = "Nebra %s Hotspot" % (variant)
+        macAddr = open("/sys/class/net/eth0/address").readline().strip().replace(":","")
+        localName = "Nebra %s Hotspot %s" % (variant, macAddr)
         self.add_local_name(localName)
         self.include_tx_power = True
         self.service_uuids = ["0fda92b2-44a2-4af2-84f5-fa682baa2b8d"]
