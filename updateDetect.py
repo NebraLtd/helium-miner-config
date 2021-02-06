76e4f68a84f73518c502e2c84e619f0760d52c01
Ryan Walmsley <Ryan@pi-supply.com>
2021-02-06 13:43:37 +0000

    Remove development files

--- updateDetect.py
+++
@@ -1,7 +0,0 @@
-import urllib.request, json, os
-supervisorAddress = str(os.environ['BALENA_SUPERVISOR_ADDRESS'])
-supervisorKey = str(os.environ['BALENA_SUPERVISOR_API_KEY'])
-supervisorAddress = "%s/v2/applications/state?apikey=%s" % (supervisorAddress, supervisorKey)
-with urllib.request.urlopen(supervisorAddress) as url:
-    data = json.loads(url.read().decode())
-    print(data)
--- wifiTests.py
+++
@@ -1,7 +0,0 @@
-import nmcli
-
-nmcli.disable_use_sudo()
-
-for network in nmcli.device.wifi():
-    if(network.ssid != "--"):
-        print(network.ssid)
