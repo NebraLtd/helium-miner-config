07fad44240e924b353a5eece7cfa8df6b4c70f14
Ryan Walmsley <ryan@ryanteck.uk>
2021-02-07 18:18:16 +0000

    And revert diagnostics back.

--- main.py
+++ main.py
@@ -799,7 +799,7 @@ def ledThreadCode():
         diagnosticsJsonFile = open("/var/data/nebraDiagnostics.json")
         diagnosticsJsonFile = json.load(diagnosticsJsonFile)
         if(diagnosticsJsonFile['PF'] is True):
-            diagnosticsStatus = False
+            diagnosticsStatus = True
         else:
             diagnosticsStatus = False
     except FileNotFoundError:
