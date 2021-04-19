ace42ee6502a4904aac92dd7d122172d59b49dae
Ryan Walmsley <ryan@ryanteck.uk>
2021-04-19 10:40:03 +0100

    Add ID to sentry

--- config_program.py
+++ config_program.py
@@ -7,6 +7,7 @@ import sentry_sdk
 
 sentryKey = os.getenv('SENTRY_CONFIG')
 sentry_sdk.init(sentryKey, traces_sample_rate=1.0)
+sentry_sdk.set_user({"id" : os.getenv('BALENA_DEVICE_UUID')})
 
 # Main imports
 import dbus
